0065327ce2f3a7ab9dbec4a699082af97a95c109
ricky-sensei <axcelcamp.teacher@gmail.com>
2023-11-03 19:26:20 +0900

    石がおけたよ

--- static/index.js
+++ static/index.js
@@ -7,13 +7,26 @@ window.onload = function(){
   let othelloWhite = "〇";
   let othelloBlack = "●";
   let othelloColor = othelloBlack;
+ 
+  
+
 
   for (let $i = 0; $tableElements.length; $i++){
     // add Event Listener
     // イベントを聞き取る人を追加するよ！
     $tableElements[$i].addEventListener("click", function(){
-      // 来週はここから
-    
+      // クリックしたいときに実行する内容
+      console.log(othelloColor);
+      $tableElements[$i].innerText = othelloColor;
+      
+      if (othelloColor == othelloBlack) {
+        othelloColor = othelloWhite;
+      }else if (othelloColor == othelloWhite) {
+        othelloColor = othelloBlack;
+      }else{
+        console.log("なんか変だよ");
+      }
+      
     });
   }
 }
