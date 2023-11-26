545fd56d5b37a7aa5320f0332880598547ce6fbb
ricky-sensei <axcelcamp.teacher@gmail.com>
2023-11-26 22:06:23 +0900

    連石の裏返り（右）成功　列が変わったときに未対応

--- static/index.js
+++ static/index.js
@@ -4,9 +4,9 @@ window.onload = function(){
   // ~という名前(name)のタグ（tag）の要素（element）をget=とってくるよ
   var $tableElements = document.getElementsByTagName("td");
   let order = true;
-  let othelloWhite = "〇";
-  let othelloBlack = "●";
-  let othelloColor = othelloBlack;
+  let White = "〇";
+  let Black = "●";
+  let turn = Black;
  
   
 
@@ -16,17 +16,43 @@ window.onload = function(){
     // イベントを聞き取る人を追加するよ！
     $tableElements[$i].addEventListener("click", function(){
       // クリックしたいときに実行する内容
-      console.log(othelloColor);
-      $tableElements[$i].innerText = othelloColor;
       
-      if (othelloColor == othelloBlack) {
-        othelloColor = othelloWhite;
-      }else if (othelloColor == othelloWhite) {
-        othelloColor = othelloBlack;
+      $tableElements[$i].innerText = turn;
+      if ($tableElements[$i + 1].innerText != "" ){
+        if ($tableElements[$i + 1].innerText != turn){
+          console.log("右隣に相手の石があるよ");
+          console.log(right($tableElements, $i, turn));
+        }
+      }
+      
+      if (turn == Black) {
+        turn = White;
+      }else if (turn == White) {
+        turn = Black;
       }else{
         console.log("なんか変だよ");
       }
       
     });
+  }
+}
+
+function right(table, i, turn){
+  var count = 1;
+  stone = ""
+  if (turn == "●"){
+    stone = "〇";
+  }else{
+    stone = "●";
+  }
+  while(table[i + count].innerText == stone){
+    table[i + count].innerText = turn;
+    count += 1;
+    
+  }
+  if (table[i + count].innerText == turn){
+    return count - 1;
+  }else{
+    return 0;
   }
 }
